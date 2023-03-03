758194ef5e000d0416b769b3683be057c727bd05
CouchMeka <meka.daniels1@gmail.com>
2023-03-03 11:58:58 -0500

    formatting code

--- src/Rows.js
+++ src/Rows.js
@@ -12,14 +12,18 @@ import ReactCardFlip from "react-card-flip";
       
  
 //page 1
-const RickDataCardFront = (props) => {
+const RickData = (props) => {
+    // set flip state for card flip 
     const [flip, setFlip] = useState(false);
+
+    //tooltip
     const renderTooltip = (props) => (
         <Tooltip  {...props}>
           Click for Stats
         </Tooltip>
       );
-    console.log(props.data)
+
+      //card mapping
     const rickMorty = props.data.map((characters, index) => {
     
 
@@ -28,6 +32,7 @@ const RickDataCardFront = (props) => {
         return (
             
    <Col key = {index}>
+    {/* CSS Styling */}
     <style type="text/css">
         {`
     
@@ -35,41 +40,43 @@ const RickDataCardFront = (props) => {
     transform: translateY(-10px);
     box-shadow: 0 20px 26px rgba(0,0,0,0.20);
     transform: scale(1.1);
-}
+     }
   
    .card {
     margin-bottom: 25px;
     margin-top: 25px;
-   }
+    }
   
   
   `}
       </style>
+      {/* card flip */}
     <ReactCardFlip isFlipped={flip} 
             flipDirection="horizontal">
     <div className='react-card'>
-    <OverlayTrigger
+      {/* overlay */}
+<OverlayTrigger
       placement="right"
       delay={{ show: 250, hide: 400 }}
       overlay={renderTooltip}
     >
+      {/*card  */}
     <Card id="enlarge" style={{ width: '18rem' }} onClick={() => setFlip(!flip)} bg="warning">
-    
       <Card.Img variant="top" src={characters.image} />
       <Card.Body>
         <Card.Title>{characters.name}</Card.Title>
         <Card.Text>
           Gender: {characters.gender} <br/>
           Species: {characters.species} <br/>
-          Status: {characters.status}
-          
+          Status: {characters.status}   
         </Card.Text>
       </Card.Body>
     </Card>
-    </OverlayTrigger>
+</OverlayTrigger>
     </div>
+    {/* reverse card size */}
     <div >
-          <div class='react-card'>
+        <div class='react-card'>
             <Card id="enlarge" style={{ width: '18rem' }} onClick={() => setFlip(!flip)} bg="info">
             <Card.Img variant="top" src={characters.image} />
             <Card.Title>Stats</Card.Title>
@@ -78,17 +85,16 @@ const RickDataCardFront = (props) => {
               Location {characters.origin.name}
             </Card>
           </div>
-        
       </div>
       </ReactCardFlip>
 </Col>
         )
     });
-
+// return card grid container
     return (
         
         <div >
-            <Container className="card-grid">
+            <Container >
                 <Row>
                     {rickMorty}
                 </Row>
@@ -106,16 +112,18 @@ const RickDataCardFront = (props) => {
 
 //page 2
 const MortyData = (props) => {
+// set state for collapsable stats
 
-    const [open, setOpen] = useState(false);
+const [open, setOpen] = useState(false);
 
-    console.log(props.page)
-    const mortyRick = props.page.map((characters, index) => {
+// card mapping
+const mortyRick = props.page.map((characters, index) => {
 
-        console.log(characters)
+        
         
         return (
-   <Col key = {index}>
+<Col key = {index}>
+    {/* CSS styling */}
      <style type="text/css">
         {`
     
@@ -123,18 +131,19 @@ const MortyData = (props) => {
     transform: translateY(-10px);
     box-shadow: 0 20px 26px rgba(0,0,0,0.20);
     transform: scale(1.1);
-}
+     }
   
    .card {
     margin-bottom: 25px;
     margin-top: 25px;
-   }
+     }
   
   
   `}
       </style>
     <div className='react-card'>
-    <Card id="enlarge" onClick={() => setOpen(!open)}
+        {/* card with collapse aria controls */}
+<Card id="enlarge" onClick={() => setOpen(!open)}
         aria-controls="example-collapse-text"
         aria-expanded={open} className="card" bg="warning" style={{ width: '18rem' }}>
       <Card.Img variant="top" src={characters.image} />
@@ -146,8 +155,9 @@ const MortyData = (props) => {
           Status: {characters.status}
         </Card.Text>
       </Card.Body>
-    </Card>
+</Card>
     </div>
+    {/* collapsable card */}
     <div style={{ minHeight: '150px', width: '10rem'  }}>
         <Collapse in={open} dimension="width">
           <div id="example-collapse-text">
@@ -162,7 +172,7 @@ const MortyData = (props) => {
 </Col>
         )
     });
-
+// return grid container
     return (
         
         <div>
@@ -204,21 +214,21 @@ render() {
         id="uncontrolled-tab-example"
         className="mb-3"
       >
-        <Tab eventKey="Page1" title="Page1">
+    <Tab eventKey="Page1" title="Page1">
         <Card  className='card'>
         <Card.Header>
         <h1>Rick and Morty Characters</h1></Card.Header>
-        <RickDataCardFront data ={data}/>
+        <RickData data ={data}/>
        </Card>
     </Tab>
-        <Tab eventKey="page2" title="Page2">
+    <Tab eventKey="page2" title="Page2">
         <Card  className='card'>
         <Card.Header>
         <h1>Rick and Morty Characters</h1></Card.Header>
          <MortyData page ={page}/>
         </Card>
         </Tab>
-      </Tabs>
+    </Tabs>
 
     </div>
   );
