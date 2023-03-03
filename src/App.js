4579dd5f68a433238e342f2e56e126beb0b46d96
CouchMeka <meka.daniels1@gmail.com>
2023-03-03 06:32:00 -0500

    pages

--- src/App.js
+++ src/App.js
@@ -6,17 +6,22 @@ import { useEffect, useState } from 'react';
 
 
 
-const DATA_URL = "https://rickandmortyapi.com/api/character"
+const DATA_URL_RICK = "https://rickandmortyapi.com/api/character"
+
+const DATA_URL_MORTY = "https://rickandmortyapi.com/api/character/?page=2"
+
+
 function App() {
 
 const [cardData, updateCards] = useState([])
+const [pageData, updatePage] = useState([])
 
   useEffect(() => {
 
     //asyncronously request data 
     // wait for promise to resolve
     // const moviesData = []
-    fetch(DATA_URL)
+    fetch(DATA_URL_RICK)
     .then((result) => result.json())
     .then((result) => {
       updateCards(result.results)
@@ -26,13 +31,28 @@ const [cardData, updateCards] = useState([])
     }, [])
 
 
+    useEffect(() => {
+
+      //asyncronously request data 
+      // wait for promise to resolve
+      // const moviesData = []
+      fetch(DATA_URL_MORTY)
+      .then((result) => result.json())
+      .then((result) => {
+        updatePage(result.results)
+  
+      });
+    
+      }, [])
+
+
 
 
 
   return (
     <div className="App">
       
-      <BasicExample data = {cardData}/>
+      <BasicExample data = {cardData} page = {pageData}/>
       
     </div>
   );
--- src/Rows.js
+++ src/Rows.js
@@ -1,9 +1,54 @@
 
 import { Component } from 'react';
-import { Container, Row, Card, Col } from 'react-bootstrap';
+import { Container, Row, Card, Col, Tab,Tabs } from 'react-bootstrap';
+
+  
+
+//page 2
+  const MortyData = (props) => {
+
+    console.log(props.page)
+    const mortyRick = props.page.map((characters, index) => {
+
+        console.log(characters)
+        
+        return (
+   <Col key = {index}>
+    <Card bg="info" style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={characters.image} />
+      <Card.Body>
+        <Card.Title>{characters.name}</Card.Title>
+        <Card.Text>
+          Gender: {characters.gender} <br/>
+          Species: {characters.species} <br/>
+          Status: {characters.status}
+        </Card.Text>
+      </Card.Body>
+    </Card>
+</Col>
+        )
+    });
+
+    return (
+        
+        <div>
+            <Container>
+                <Row>
+                    {mortyRick}
+                </Row>
+            </Container>
+         </div>
+        
+        
+    )
+
+}
 
 
 
+
+
+//page 1
 const RickData = (props) => {
 
     console.log(props.data)
@@ -43,18 +88,38 @@ const RickData = (props) => {
 
 }
 
+
+
+
 class BasicExample extends Component {
 
 
 
 render() {
-    const {data} = this.props;
+    const {data, page } = this.props;
   return (
     <div>
+  <Tabs
+        defaultActiveKey="profile"
+        id="uncontrolled-tab-example"
+        className="mb-3"
+      >
+        <Tab eventKey="Page1" title="Page1">
         <Card  className='card'>
-        <Card.Header><h1>Rick and Morty Characters</h1></Card.Header>
-    <RickData data ={data}/>
-    </Card>
+        <Card.Header>
+        <h1>Rick and Morty Characters</h1></Card.Header>
+        <RickData data ={data}/>
+       </Card>
+    </Tab>
+        <Tab eventKey="page2" title="Page2">
+        <Card  className='card'>
+        <Card.Header>
+        <h1>Rick and Morty Characters</h1></Card.Header>
+         <MortyData page ={page}/>
+        </Card>
+        </Tab>
+      </Tabs>
+
     </div>
   );
 }
